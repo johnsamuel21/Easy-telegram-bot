b162d334d4e2086af4e10697c4ee295808c642a2
John Samuel J <jjohnsamuel21@gmail.com>
2020-08-22 08:34:29 +0530

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -6,15 +6,17 @@ import json
 import requests
 import re
 from bs4 import BeautifulSoup as bso
+import os
 
+
+token = "YOUR_TOKEN"
 
 def handle(msg):
     user_name = msg["from"]["first_name"]
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    chat_id = msg['chat']['id']
+    txt = msg["text"]
 
-    if content_type == "text":
-        txt = msg["text"]
-
+    if chat_id:
         txt = txt.lower()
         txt = txt.replace("?","")
         txt = txt.replace("/","")
@@ -28,11 +30,6 @@ def handle(msg):
         if txt == "start":
             bot.sendMessage(chat_id,"Hi {}, Use \"/help\", to list out the possible actions I can perform.\n Have a great day:) ".format(user_name))
 
-        elif re.search(r".*@.*bot",txt):
-            txt = re.sub(r'@.*bot', '', txt)
-            txt = txt.strip()
-            bot.sendMessage(chat_id,"I'm listening...")
-
         elif txt == "help":
             bot.sendMessage(chat_id,"Get comfortable with the following commands.\n /joke - To get random jokes.\n /numfacts <type_a_number> - to get various number facts.\n /insults - to get some offensive insults(Think before you do this).\n /quote - to get random quotes of life")
 
@@ -44,12 +41,18 @@ def handle(msg):
                 response = requests.get(url)
                 bot.sendMessage(chat_id, response.content)
             else:
-                bot.sendMessage(chat_id, "please enter a valid number")
+                bot.sendMessage(chat_id, "please enter a valid number after the command /numfacts")
 
         elif "insults" in txt:
             url = "https://insult.mattbas.org/api/insult"
             response = requests.get(url).text
             bot.sendMessage(chat_id,response)
+
+        elif re.search(".*dog.*",txt):
+            response = requests.get("https://api.thedogapi.com/v1/images/search")
+            [jsonformat] = response.json()
+            final = jsonformat["url"]
+            bot.sendPhoto(chat_id,final)
 
 
         elif txt == "quote" or txt == "quotes":
@@ -59,7 +62,7 @@ def handle(msg):
             ans = soup.find("h2").get_text()
             bot.sendMessage(chat_id,ans)
 
-        elif re.match("hi+",txt) or txt == "hello" or re.match("hlo+",txt):
+        elif txt == "hi" or txt == "hello" or txt == "hlo":
             lst = ["CAACAgIAAxkBAAEBO1ZfPudTYMdWRC8SkiZ7jIx9iBlR1QACSAQAAs7Y6AvADEi_kJvbjBsE","CAACAgIAAxkBAAEBO1JfPua6EIlXk1XD3FtJlOvntSOTqAACKQEAAhZCawqscJjQ9N192BsE","CAACAgIAAxkBAAEBO1hfPueolJ-NddSsbNn5duZC8u9cMQACEwADwDZPE6qzh_d_OMqlGwQ"]
             bot.sendMessage(chat_id,"Hi!!!!")
             bot.sendSticker(chat_id,random.choice(lst))
@@ -98,6 +101,25 @@ def handle(msg):
             bot.sendMessage(chat_id,"Sry, better find someone, I'm busy.")
             bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBOV1fPS3OzP1Izk8NxSHWYxlGKSLGZAACFgADwDZPE2Ah1y2iBLZnGwQ")
 
+        elif txt == "lol" or txt == "funny" or re.match("haha.*",txt) or txt == "nice joke" or re.match("hehe.*",txt):
+            bot.sendMessage(chat_id,"I am glad that you are happy.")
+            bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBO8FfP68gfMVwKyS5yZEl4f6N5sXvJwACWgUAAj-VzAobFrmFvSDDnRsE")
+
+        elif txt == "r u single":
+            singlelst = ["Why do you ask? Are you going to marry me?","God is still busy writing my love story.","I’m married to video games!","Two words: Trust issues!","I'm allergic to bullshit."]
+            select = random.choice(singlelst)
+            bot.sendMessage(chat_id,"Yes, "+select)
+
+        elif re.search("don.?t talk",txt):
+            bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBO8VfP7LlTsI2sbO7mkQIdRxjU22X8AACeAUAAj-VzAqq_QjcS2pGOhsE")
+
+        elif "nice" in txt or "awesome" in txt or "great" in txt or "cool" in txt:
+            bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBO_5fP-jY83416S6pATmwgcjNpw5UAwACBgADwDZPE8fKovSybnB2GwQ")
+
+        elif txt == "where r u":
+            bot.sendMessage(chat_id,"In your heart.")
+            bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBPR9fQIfXoA7qnBu9MeQ_D6L3fXFQIQACTAEAAhZCawqG_ywJS5anJxsE")
+
         elif txt == "joke" or txt == "tell a joke" or txt == "jokes":
             jokelst = ["I ate a clock yesterday, it was very time-consuming.","What's the difference between ignorance and apathy? A. I don’t know and I don’t care.",
             "I recently decided to sell my vacuum cleaner as all it was doing was gathering dust.","250 lbs here on Earth is 94.5 lbs on Mercury. No, I'm not fat. I’m just not on the right planet."
@@ -109,19 +131,25 @@ def handle(msg):
             bot.sendMessage(chat_id,joke)
 
         elif txt == "thanks" or txt == "tnks" or txt == "tanx" or txt == 'thank u' or txt == "tanq" or txt == "thanq":
-            bot.sendMessage(chat_id,"My pleasure (ᵔᴥᵔ)")
+            bot.sendMessage(chat_id,"My pleasure (ᵔᴥᵔ).")
             bot.sendDocument(chat_id,"https://media.giphy.com/media/fwFHkV9I9nWVO/giphy.gif")
 
         else:
-            bot.sendMessage(chat_id,"Sry, my programmer didn't finish some part of codings. I apologize for his mistakes.")
+            bot.sendMessage(chat_id,"Neengal solvathil,sirithalavu kuda puriyavillai. Manufacturing defect.")
             bot.sendSticker(chat_id,"CAACAgIAAxkBAAEBO1BfPuKgFKLDHVGNBxL6oR5K062vXAACXgEAAhZCawqE_ArUAgLZUBsE")
 
 
 
-
-token = "YOUR_TOKEN"
 bot = telepot.Bot(token)
 bot.message_loop(handle)
 
 while 1:
-    time.sleep(10)
+    try:
+        time.sleep(10)
+
+    except KeyboardInterrupt:
+        print('\n Program interrupted')
+        exit()
+
+    except:
+        print('Other error or exception occured!')
